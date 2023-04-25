ab1d24863c7b27c39106a70f4a5f137a733c92a2
zyme <lolrichyisswag@gmail.com>
2023-04-25 16:51:57 -0500

    rewrote some timer logic

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,26 @@
-import Eris, { Guild, Member, Message } from "eris";
+import Eris, { Member, VoiceChannel } from "eris";
 import * as dotenv from 'dotenv'
 dotenv.config()
 let token: string = process.env.TOKEN
-let channelIds: string[] = [];
 const bot = new Eris.Client(token);
 
 bot.on("ready", () => {
     console.log("Ready!");
 });
 
-bot.on("messageCreate", (msg: Message) => {
-    if (msg.content === "!ping") {
-        bot.createMessage(msg.channel.id, "Pong!");
-    }
-});
 
-bot.on("voiceChannelJoin", (member: Member, server: Guild) => {
-    console.log(member);
-    let VcChannel: string = server.id;
-    if (channelIds.includes(VcChannel)) {
-        bot.createMessage(VcChannel, `A timer is already set for this channel, <@${member.id}>`);
-        return;
+bot.on("voiceChannelJoin", (member: Member, vc: VoiceChannel) => {
+    let usersInVc = Array.from(vc.voiceMembers.values()).length // converts iterable to array, then gets value of length
+    let VcChannel: string = vc.id;
+    // designed to prevent double pings
+    if (usersInVc > 1) {
+        console.log("Timer has not started, as one is already ongoing for this channel.")
+    } else {
+        bot.createMessage(VcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
+        setTimeout(() => {
+            bot.createMessage(VcChannel, "A call has started! <@&1091919355262025781>");
+        }, 60000);
     }
-    channelIds.push(VcChannel);
-    bot.createMessage(VcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
-    setTimeout(() => {
-        bot.createMessage(VcChannel, "A call has started! <@&1091919355262025781>");
-        const index = channelIds.indexOf(VcChannel);
-        if (index > -1) {
-            channelIds.splice(index, 1);
-        }
-    }, 60000);
 });
 
 bot.connect();
