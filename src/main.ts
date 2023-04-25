3ebcb37c712dc00f77d830fb4e9ab0fcb620a398
zyme <lolrichyisswag@gmail.com>
2023-04-24 22:41:32 -0500

    I CANT BELIEVE IT ACTUALLY WORKED

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Eris, { Guild, Member, Message } from "eris";
 import * as dotenv from 'dotenv'
 dotenv.config()
 let token: string = process.env.TOKEN
+let channelIds: string[] = [];
 const bot = new Eris.Client(token);
 
 bot.on("ready", () => {
@@ -16,12 +17,21 @@ bot.on("messageCreate", (msg: Message) => {
 
 bot.on("voiceChannelJoin", (member: Member, server: Guild) => {
     console.log(member);
-    let VcChannel: string = server.id
-    bot.createMessage(VcChannel, `Waiting 1 minute to ping. <@${member.id}>`)
+    let VcChannel: string = server.id;
+    if (channelIds.includes(VcChannel)) {
+        bot.createMessage(VcChannel, `A timer is already set for this channel, <@${member.id}>`);
+        return;
+    }
+    channelIds.push(VcChannel);
+    bot.createMessage(VcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
     setTimeout(() => {
-        bot.createMessage(VcChannel, "A call has started! <@&1091919355262025781>")
+        bot.createMessage(VcChannel, "A call has started! <@&1091919355262025781>");
+        const index = channelIds.indexOf(VcChannel);
+        if (index > -1) {
+            channelIds.splice(index, 1);
+        }
     }, 60000);
-})
+});
 
 bot.connect();
 
