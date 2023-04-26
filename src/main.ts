0149207f255ebf50f270b7766571dbf025778bb2
zyme <lolrichyisswag@gmail.com>
2023-04-25 22:02:45 -0500

    I have brain rot

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Eris, { Member, VoiceChannel } from "eris";  
+import { readdirSync } from "fs";
 import * as dotenv from 'dotenv'
 dotenv.config()
 let token: string = process.env.TOKEN
@@ -10,18 +11,20 @@ bot.on("ready", () => {
 
 bot.on("voiceChannelJoin", (member: Member, vc: VoiceChannel) => {
     let usersInVc = Array.from(vc.voiceMembers.values()).length // converts iterable to array, then gets value of length
-    let VcChannel: string = vc.id;
+    let vcChannel: string = vc.id;
     console.log(`${member.id} has joined the channel ${vc.id}`)
     // designed to prevent double pings
     if (usersInVc > 1) {
         console.log("Timer has not started, as one is already ongoing for this channel.")
     } else {
-        bot.createMessage(VcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
+        bot.createMessage(vcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
         setTimeout(() => {
-            bot.createMessage(VcChannel, "A call has started! <@&1091919355262025781>");
+            bot.createMessage(vcChannel, "A call has started! <@&1091919355262025781>");
         }, 60000);
     }
 });
+
+
 
 bot.connect();
 
