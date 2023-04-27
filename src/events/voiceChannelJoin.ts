33a7de5205ff5e4d8169366100692ef0d49fcf70
zyme <lolrichyisswag@gmail.com>
2023-04-27 18:21:02 -0500

    no longer pings even if the user left

--- src/events/voiceChannelJoin.ts
+++ src/events/voiceChannelJoin.ts
@@ -3,12 +3,14 @@ import { Client, Member, VoiceChannel } from "eris";
 export default async (bot: Client, member: Member, vc: VoiceChannel) => {
   let usersInVc = Array.from(vc.voiceMembers.values()).length; // converts iterable to array, then gets value of length
   let vcChannel: string = vc.id;
-
+  async function updateVcUserCount(){
+    usersInVc = Array.from(vc.voiceMembers.values()).length;
+  }
   async function delay(ms: number) {
     await new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  console.log(`${member.id} has joined the channel ${vc.id} in the server ${vc.guild.id}`);
+  console.log(`[Discord] ${member.id} has joined the channel ${vc.id} in the server ${vc.guild.id}`);
 
   // designed to prevent double pings
   if (usersInVc > 1) {
@@ -16,6 +18,11 @@ export default async (bot: Client, member: Member, vc: VoiceChannel) => {
   } else {
     bot.createMessage(vcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
     await delay(60000);
-    bot.createMessage(vcChannel, "A call has started! <@&1091919355262025781>");
+    updateVcUserCount()
+    if (usersInVc > 0) {
+      bot.createMessage(vcChannel, "A call has started! <@&1091919355262025781>");
+    } else{
+      console.log("[Discord] Timer initiator no longer present, ping not sent.")
+    }
   }
 };
