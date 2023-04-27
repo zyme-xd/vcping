407f514a784fc11b9f8f022c4f0015fb7a7ff1b4
zyme <lolrichyisswag@gmail.com>
2023-04-27 16:22:11 -0500

    small changes

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,6 +7,6 @@ export const info = {
   options: [],
 };
 
-export async function run(_client: Client, interaction: CommandInteraction) {
+export async function run(_client: Client, interaction: CommandInteraction): Promise<void> {
   await interaction.createMessage("Pong!");
 }
--- src/events/voiceChannelJoin.ts
+++ src/events/voiceChannelJoin.ts
@@ -3,16 +3,19 @@ import { Client, Member, VoiceChannel } from "eris";
 export default async (bot: Client, member: Member, vc: VoiceChannel) => {
   let usersInVc = Array.from(vc.voiceMembers.values()).length; // converts iterable to array, then gets value of length
   let vcChannel: string = vc.id;
+
   async function delay(ms: number) {
-    await new Promise(resolve => setTimeout(resolve,ms))
+    await new Promise((resolve) => setTimeout(resolve, ms));
   }
-  console.log(`${member.id} has joined the channel ${vc.id}`);
+
+  console.log(`${member.id} has joined the channel ${vc.id} in the server ${vc.guild.id}`);
+
   // designed to prevent double pings
   if (usersInVc > 1) {
     console.log("Timer has not started, as one is already ongoing for this channel.");
   } else {
     bot.createMessage(vcChannel, `Waiting 1 minute to ping. <@${member.id}>`);
-    await delay(60000)
+    await delay(60000);
     bot.createMessage(vcChannel, "A call has started! <@&1091919355262025781>");
   }
 };
