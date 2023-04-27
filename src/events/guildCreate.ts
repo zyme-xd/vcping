453f5402c408b601158f52a77276908b2ac2b71d
zyme <lolrichyisswag@gmail.com>
2023-04-27 17:43:24 -0500

    better comments

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -8,12 +8,12 @@ export default (_client: Client, guild: Guild) => {
   console.log(guild.id);
   const dataFilePath = path.join(__dirname, "..", "data.json");
 
-  // Check if the data file exists
+  // check if the data file exists
   const fileExists = fs.existsSync(dataFilePath);
 
-  // If the file does not exist, create it
+  // if the file does not exist, create it
   if (!fileExists) {
-    // Create an empty JSON object and write it to the file
+    // create an empty JSON object and write it to the file
     const initialData: { [key: string]: ServerObj } = {};
     try {
       fs.writeFileSync(dataFilePath, JSON.stringify(initialData));
@@ -22,11 +22,14 @@ export default (_client: Client, guild: Guild) => {
       console.error(`Error creating ${dataFilePath}: ${err}`);
     }
   }
+  // parse the contents of the data file into a JSON object
   const data: { [key: string]: ServerObj } = JSON.parse(fs.readFileSync(dataFilePath, "utf8"));
-
+  
+  // create an empty server object for this guild and add it to the data object
   const serverObj: ServerObj = {};
-
   data[guild.id] = serverObj;
+  
+  //write updated file 
   try {
     fs.writeFileSync(dataFilePath, JSON.stringify(data));
     console.log(`Updated ${dataFilePath}`);
