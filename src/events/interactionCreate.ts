6c8f63451b987874a5acbe9a6351635b31627a5f
zyme <lolrichyisswag@gmail.com>
2023-04-27 17:53:48 -0500

    more types

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -3,7 +3,7 @@ import { commandlist } from "../main.js";
 
 // create default function
 export default (client: Client, interaction: CommandInteraction) => {
-  const { data } = interaction || {};
+  const { data }: CommandInteraction = interaction || {};
   const command = commandlist.get(data?.name); // get command from list based on name
   return command?.run(client, interaction); // call run method (if it exists)
 };
