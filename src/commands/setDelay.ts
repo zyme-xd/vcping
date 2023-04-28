e6e5467e8fc9075de117f104d196329973abcca7
zyme <lolrichyisswag@gmail.com>
2023-04-27 22:12:00 -0500

    commands exclusive to administrator perm

--- src/commands/setDelay.ts
+++ src/commands/setDelay.ts
@@ -1,12 +1,17 @@
 import { Client, CommandInteraction } from "eris";
 
 export const info = {
-  name: "setDelay",
+  name: "setdelay",
   description: "Set the delay VcPing waits before pinging in your server.",
   disabled: false,
   options: [],
 };
 
 export async function run(_client: Client, interaction: CommandInteraction): Promise<void> {
+  const isAdmin = interaction.member?.permissions.has("administrator");
+  if (!isAdmin) {
+    await interaction.createMessage("You do not have permission to run this command!!");
+    return
+  }
   await interaction.createMessage("Unimplemented.");
 }
--- src/commands/setRole.ts
+++ src/commands/setRole.ts
@@ -1,12 +1,17 @@
 import { Client, CommandInteraction } from "eris";
 
 export const info = {
-  name: "setRole",
+  name: "setrole",
   description: "Set the role VcPing notifies for your server.",
   disabled: false,
   options: [],
 };
 
 export async function run(_client: Client, interaction: CommandInteraction): Promise<void> {
-  await interaction.createMessage("Unimplemented.");
+    const isAdmin = interaction.member?.permissions.has("administrator");
+    if (!isAdmin) {
+      await interaction.createMessage("You do not have permission to run this command!!");
+      return
+    }
+    await interaction.createMessage("Unimplemented.");
 }
