04036798d39c85dab87113c380b0f4ad946a0a52
zyme <lolrichyisswag@gmail.com>
2023-04-28 23:21:46 -0500

    added sum checks (THIS MADE ME WANNA KMS)

--- src/commands/setRole.ts
+++ src/commands/setRole.ts
@@ -1,20 +1,45 @@
 import { Client, CommandInteraction } from "eris";
 
+// THIS IS THE WORST THING EVER
+function isRoleOption(option: any): option is { value: string } {
+  return option && typeof option.value === "string";
+}
+
+function getRoleValue(optionsData: any): string | null {
+  if (Array.isArray(optionsData) && optionsData.length > 0 && isRoleOption(optionsData[0])) {
+    return optionsData[0].value.toString();
+  } else {
+    return null;
+  }
+}
+
 export const info = {
   name: "setrole",
   description: "Set the role VcPing notifies for your server.",
   disabled: false,
-  options: [{ name: "roleid", description: "The role ID.", type: 3 , required: true}],
+  options: [{ name: "roleid", description: "The role ID.", type: 3, required: true }],
 };
 
-export async function run(_client: Client, interaction: CommandInteraction): Promise<void> {
+export async function run(client: Client, interaction: CommandInteraction): Promise<void> {
   const optionsData = interaction.data.options;
-  console.log(optionsData?.[0]) // im stuck here
-  await interaction.acknowledge(64);
   const isAdmin = interaction.member?.permissions.has("administrator");
+  const roleId = getRoleValue(optionsData);
+  const roles = client.guilds?.get(`${interaction.guildID}`)?.roles;
+
+  await interaction.acknowledge(64);
+
   if (!isAdmin) {
     await interaction.createFollowup("You do not have permission to run this command!!");
     return;
   }
-  await interaction.createFollowup("Unimplemented.");
+
+  if (roleId) {
+    let checkedValue = parseInt(roleId);
+    if (Number.isNaN(checkedValue) || !roles?.has(roleId)) {
+      // checks if input is a valid number and if it actually exists
+      await interaction.createFollowup(`Invalid input.`);
+      return;
+    }
+    await interaction.createFollowup(`Unimplemented. ${checkedValue}`);
+  }
 }
